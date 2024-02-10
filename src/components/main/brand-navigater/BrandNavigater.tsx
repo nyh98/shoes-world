9e8c5d1a3dab38adcb4fec7d559addd09e481982
nyh <nsi3358@naver.com>
2024-02-10 14:40:39 +0900

    브랜드관 현재 브랜드에 따라 메뉴바 스타일 지정

--- src/components/main/brand-navigater/BrandNavigater.tsx
+++ src/components/main/brand-navigater/BrandNavigater.tsx
@@ -1,28 +1,50 @@
 import React from 'react';
 import styles from './BrandNavigater.module.css';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 export default function BrandNavigater() {
+  const { brandName } = useParams();
+
   return (
     <div className={styles.container}>
-      <p className={styles.navigater}>
+      <p
+        className={
+          brandName === 'adidas'
+            ? styles['current-navigater']
+            : styles.navigater
+        }
+      >
         <Link to={'/shop/adidas'} className={styles.link}>
           adidas
         </Link>
       </p>
-      <p className={styles.navigater}>
+      <p
+        className={
+          brandName === 'Jordan'
+            ? styles['current-navigater']
+            : styles.navigater
+        }
+      >
         <Link to={'/shop/Jordan'} className={styles.link}>
-          Jordan{' '}
+          Jordan
         </Link>
       </p>
-      <p className={styles.navigater}>
+      <p
+        className={
+          brandName === 'Nike' ? styles['current-navigater'] : styles.navigater
+        }
+      >
         <Link to={'/shop/Nike'} className={styles.link}>
-          Nike{' '}
+          Nike
         </Link>
       </p>
-      <p className={styles.navigater}>
+      <p
+        className={
+          brandName === 'Vans' ? styles['current-navigater'] : styles.navigater
+        }
+      >
         <Link to={'/shop/Vans'} className={styles.link}>
-          Vans{' '}
+          Vans
         </Link>
       </p>
     </div>
